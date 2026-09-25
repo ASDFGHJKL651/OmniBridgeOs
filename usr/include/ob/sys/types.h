817cbab531499e535ef66a98f12b658ff2e631ba
ASDFGHJKL651 <3847216385@qq.com>
2026-09-25 16:11:56 +0800

    Delete usr directory

--- usr/include/ob/sys/types.h
+++
@@ -1,9 +0,0 @@
-#ifndef OB_USER_SYS_TYPES_H
-#define OB_USER_SYS_TYPES_H
-#include <stdint.h>
-typedef uint32_t uid_t;
-typedef uint32_t gid_t;
-typedef int32_t  pid_t;
-typedef int64_t  off_t;
-typedef uint64_t size_t;
-#endif
